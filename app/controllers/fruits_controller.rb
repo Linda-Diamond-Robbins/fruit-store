5e1ff622b23e90653bb148a9c175d2a2c502bdbc
Linda-Diamond-Robbins <lindadiamondrobbins@gmail.com>
2016-11-20 11:41:05 -0600

    Made html.erb files

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -1,11 +1,58 @@
 class FruitsController < ApplicationController
   def index
-    @fruits = Fruit.all
+    sort_attribute = params[:sort_by]
+    @fruits = Fruit.order(sort_attribute)
     render 'index.html.erb'
   end
-
+   
   def show
     @fruit = Fruit.find_by(id: params[:id])
     render 'show.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @fruit = Fruit.new(
+      name: params["name"],
+      price: params["price"],
+      image: params["image"],
+      description: params["description"]
+    )
+    @fruit.save
+    redirect_to "/fruits/#{@fruit.id}"
+  end
+
+  def show
+    fruit_id = params[:id]
+    @fruits = Fruit.find_by(id: fruit_id)
+    render 'show.html.erb'
+  end
+
+  def edit
+    fruit_id = params[:id]
+    @fruit = Fruit.find_by(id: fruit_id)
+    render 'edit.html.erb'
+  end
+
+  def update
+    fruit_id = params[:id]
+    @fruit = Fruit.find_by(id: fruit_id)
+    @fruit.name = params[:name]
+    @fruit.price = params[:price]
+    @fruit.image = params[:image]
+    @fruit.description = params[:description]
+    @fruit.save
+    flash[:success] = "Fruit successfully updated!"
+    redirect_to "/fruits/#{@fruit.id}"
+  end
+
+  def destroy
+    fruit_id = params[:id]
+    @fruit = Fruit.find_by(id: fruit_id)
+    @fruit.destroy
+    redirect_to "/fruits"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get '/fruits' => 'fruits#index'
+  get '/fruits/new' => 'fruits#new'
+  post '/fruits' => 'fruits#create”'
   get '/fruits/:id' => 'fruits#show'
+  get '/fruits/:id/edit' => 'fruits#edit'
+  patch '/fruits/:id' => 'fruits#update'
+  delete '/fruits/:id' => 'fruits#destroy'
 end
+
+
